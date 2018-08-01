e5b16edea14cf8d31daf76f82a4425b53aea8327
Freddie Wu <freddie.wu@capitaledge.cn>
2018-08-01 17:13:39 +0800

    WIP: test build - not going yet...

--- msvc/tf.cpp
+++ msvc/tf.cpp
@@ -4,19 +4,6 @@
 #include "stdafx.h"
 #include "tf.h"
 
-
-// This is an example of an exported variable
-TF_API int ntf=0;
-
-// This is an example of an exported function.
-TF_API int fntf(void)
-{
-    return 42;
-}
-
-// This is the constructor of a class that has been exported.
-// see tf.h for the class definition
-Ctf::Ctf()
-{
-    return;
+extern "C" {
+#	include "../c/tf.c"
 }
